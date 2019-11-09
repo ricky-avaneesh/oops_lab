ee6fb63a32aed0c45eb1460c05d477ec394fac40
ricky-avaneesh <avaneeshmaithil@gmail.com>
2019-11-10 00:36:50 +0530

    Program to demonstrate usability of static data members

--- 12_static_data_in_a_class.cpp
+++ 12_static_data_in_a_class.cpp
@@ -1,12 +1,34 @@
-//                    Program to store a phone number in given format	             //
+//                 Program to demonstrate usability of static data members	         //
 //***********************************************************************************//
 //                    Copyright 2019, @ Avaneesh, All Rights reserved.               //
 //***********************************************************************************//
 #include<iostream>
 using namespace std;
+class cont
+{
+public:
+	static int cnt;
+	cont()
+	{
+		cnt++;
+	}
+	~cont()
+	{
+		cnt--;
+	}
+	static void showcnt()
+	{
+		cout<<"No. of objects = "<<cnt<<endl;
+	}
+};
+int cont::cnt=0;
 int main()
 {
 	
-	
+	cont a;
+	cont v;
+	v.showcnt();
+	a.showcnt();
+
 	return 0;
 }
